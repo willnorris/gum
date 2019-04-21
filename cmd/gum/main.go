8fdbb6131f88bbaa5fa31a95eef317c5d975fbc5
Will Norris <will@willnorris.com>
2019-04-21 22:41:59 +0000

    update URL for BSD license

--- cmd/gum/main.go
+++ cmd/gum/main.go
@@ -1,3 +1,9 @@
+// Copyright 2014 Google Inc. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/BSD-3-Clause
+
 // The gum binary starts up a gum server.  See the usage text for configuration
 // options.
 package main
